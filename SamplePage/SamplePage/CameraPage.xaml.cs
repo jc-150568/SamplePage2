1bc8df22f85967881a7d13f6b49c6d3f1a5cff49
遼太郎 金 <150568@jc-21.jp>
2017-11-24 09:46:56 +0900

    プロジェクト ファイルを追加します。

---
+++ SamplePage/SamplePage/CameraPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+using ZXing.Mobile;
+using ZXing.Net.Mobile.Forms;
+
+namespace SamplePage
+{
+    
+    public partial class CameraPage : ContentPage
+    {
+        public CameraPage()
+        {
+            var scanPage = new ZXingScannerPage()
+            {
+                DefaultOverlayTopText = "バーコードを読み取ります",
+                DefaultOverlayBottomText = "",
+            };
+
+            // スキャナページを表示
+            await Navigation.PushAsync(scanPage);
+
+            scanPage.OnScanResult += (result) =>
+            {
+                // スキャン停止
+                scanPage.IsScanning = false;
+
+                // PopAsyncで元のページに戻り、結果をダイアログで表示
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopAsync();
+                    await DisplayAlert("スキャン完了", result.Text, "OK");
+                });
+
+                //scanedData.Add(result.Text);
+            };
+        }
+    }
+}
